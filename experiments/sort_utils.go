66393f17b8da303fe4ad86562d7711d1390eb55c
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2022-02-27 19:43:10 +0530

    Remove the file that's no longer used

--- experiments/sort_utils.go
+++
@@ -1,8 +0,0 @@
-package main
-
-// IntSlice attaches the methods of Interface to []int, sorting in increasing order.
-type Int64Slice []int64
-
-func (x Int64Slice) Len() int             { return len(x) }
-func (x Int64Slice) Less(i, j int64) bool { return x[i] < x[j] }
-func (x Int64Slice) Swap(i, j int64)      { x[i], x[j] = x[j], x[i] }
